7e51131ae8d2036f9d659eadda01dc352cb92206
Lim Chun Leng <limchunleng@gmail.com>
2022-06-04 12:52:29 +0900

    problem 16

--- 0016_3sum_closest/solve.py
+++ 0016_3sum_closest/solve.py
@@ -2,26 +2,30 @@ from typing import Optional
 
 
 class Solution():
-    def three_sum_closest(self, nums: list[int], target: int) -> Optional[int]:
+    def threeSumClosest(self, nums: list[int], target: int) -> Optional[int]:
         if len(nums) < 3:
             return None
 
-        nums = sorted(nums)
+        nums.sort()
         ans = nums[0] + nums[1] + nums[2]
+        diff = abs(ans - target)
 
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                for k in range(j+1,len(nums)):
-                    s = nums[i] + nums[j] + nums[k]
-                    if abs(s - target) < abs(ans - target):
-                        ans = s
-                    if s > target:
-                        break
+        for i, n in enumerate(nums[:-2]):
+            if i > 0 and n == nums[i - 1]:
+                continue
+
+            l,r = i + 1, len(nums) - 1
+
+            while l < r:
+                s = n + nums[l] + nums[r]
+                d = abs(s - target)
+                if d < diff:
+                    ans = s
+                    diff = d
+
+                if s < target:
+                    l += 1
+                else:
+                    r -= 1
 
         return ans
-
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.three_sum_closest([0,0,0], 2))
-    print(sol.three_sum_closest([-1,2,1,-4], 1))
-    print(sol.three_sum_closest([-4,-3,-1,2,3], 0))
