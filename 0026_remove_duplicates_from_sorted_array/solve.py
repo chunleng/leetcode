23864399278acd35963d9c15220b478c8e8fed96
Lim Chun Leng <limchunleng@gmail.com>
2022-06-04 22:28:56 +0900

    problem 26

---
+++ 0026_remove_duplicates_from_sorted_array/solve.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
+        p = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[p] = nums[i]
+                p += 1
+        return p
